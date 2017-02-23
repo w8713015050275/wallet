f02a4adb2aee0e2894364c32a72acb265cffd287
Li Junying <lijunying@le.com>
2017-02-23 14:38:45 +0800

    131-Lepay: Account : sharedFerences
    Ticket: LEUIAPP-265
    
    Root cause: Multi process read sharedprefer data is not updated
    
    Solution: get Preferences use MODE_MULTI_PROCESS
    
    Change-Id: If7563ce81ad1ec6cd061feee849708aca1d92c53
    Signed-off-by: Li Junying <lijunying@le.com>

--- LePay/src/com/letv/wallet/account/utils/AccountUtils.java
+++ LePay/src/com/letv/wallet/account/utils/AccountUtils.java
@@ -1,7 +1,5 @@
 package com.letv.wallet.account.utils;
 
-import android.content.SharedPreferences;
-
 import com.letv.wallet.account.aidl.v1.AccountConstant;
 import com.letv.wallet.common.util.AccountHelper;
 import com.letv.wallet.common.util.DigestUtils;
@@ -14,18 +12,10 @@ import com.letv.wallet.common.util.SharedPreferencesHelper;
 
 public class AccountUtils {
     public static boolean hasCreatedAccount() {
-        SharedPreferences sharedPreferences = SharedPreferencesHelper.getUserIdPreferences(AccountConstant.LEPAY_PKG);
-        if (sharedPreferences != null) {
-            return sharedPreferences.getBoolean(DigestUtils.getMd5_30(AccountHelper.getInstance().getUid() + AccountConstant.SHAREDPREFERENCES_CREATE_ACCOUNT_SUFFIX), false);
-        }
-        return false;
+        return SharedPreferencesHelper.getBoolean(DigestUtils.getMd5_30( AccountHelper.getInstance().getUid() + AccountConstant.SHAREDPREFERENCES_CREATE_ACCOUNT_SUFFIX), false);
     }
 
     public static boolean hasVerifyAccount() {
-        SharedPreferences sharedPreferences = SharedPreferencesHelper.getUserIdPreferences(AccountConstant.LEPAY_PKG);
-        if (sharedPreferences != null) {
-            return sharedPreferences.getBoolean(DigestUtils.getMd5_30(AccountHelper.getInstance().getUid() + AccountConstant.SHAREDPREFERENCES_VERIFY_ACCOUNT_SUFFIX), false);
-        }
-        return false;
+        return SharedPreferencesHelper.getBoolean(DigestUtils.getMd5_30(AccountHelper.getInstance().getUid() + AccountConstant.SHAREDPREFERENCES_VERIFY_ACCOUNT_SUFFIX), false);
     }
 }
